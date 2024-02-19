3680f2190efc33e7fbd1cb4dcd04d0a7e1041b1f
siyeol97 <leesi2830@naver.com>
2024-02-19 14:38:45 +0900

    🚨 Fix: eslint configure

---
+++ .eslintrc.cjs
@@ -0,0 +1,26 @@
+module.exports = {
+  root: true,
+  env: { node: true, browser: true, es2020: true },
+  extends: [
+    'next',
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'prettier'],
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto',
+        trailingComma: 'es5',
+        singleQuote: true,
+        jsxSingleQuote: true,
+      },
+    ],
+    'react/react-in-jsx-scope': 'off',
+    //'react-hooks/exhaustive-deps': 'off',
+  },
+};

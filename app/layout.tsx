7b53f72811fa3c7333489ada638c05514edfdc90
siyeol97 <leesi2830@naver.com>
2024-02-19 16:38:27 +0900

    💄 Style: add Navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
 import Header from './components/Header';
+import Navbar from './components/Navbar';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -19,7 +20,10 @@ export default function RootLayout({
     <html lang='ko'>
       <body className={inter.className}>
         <Header />
-        {children}
+        <div style={{ display: 'flex' }}>
+          <Navbar />
+          {children}
+        </div>
       </body>
     </html>
   );

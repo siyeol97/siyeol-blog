25bf3b3916d529e7267d84cbbd942e9d0eed6646
siyeol97 <leesi2830@naver.com>
2024-02-24 20:42:00 +0900

    ✨ Feat: 글 발행시 유저 정보 저장

--- app/components/Session.tsx
+++ app/components/Session.tsx
@@ -1,21 +1,16 @@
 import Link from 'next/link';
 import LoginButton from './LoginButton';
-import { getServerSession } from 'next-auth';
-import { authOptions } from '@/pages/api/auth/[...nextauth]';
+
 import LogoutButton from './LogoutButton';
+import getUserSession from '@/utils/getUserSession';
 
 export default async function Session() {
-  const session = await getServerSession(authOptions);
-  const userData = {
-    user: session?.user?.name,
-    email: session?.user?.email,
-    image: session?.user?.image,
-  };
+  const userData = await getUserSession();
   return (
     <div>
-      {session ? (
+      {userData.name ? (
         <>
-          <span>{userData.user}</span>
+          <span>{userData.name}</span>
           <span>{userData.email}</span>
           <Link href={'/write'}>새 글 쓰기</Link>
           <LogoutButton />
---
+++ utils/getUserSession.ts
@@ -0,0 +1,14 @@
+import { authOptions } from '@/pages/api/auth/[...nextauth]';
+import { getServerSession } from 'next-auth';
+
+const getUserSession = async () => {
+  const session = await getServerSession(authOptions);
+  const userData = {
+    name: session?.user?.name,
+    email: session?.user?.email,
+    image: session?.user?.image,
+  };
+  return userData;
+};
+
+export default getUserSession;

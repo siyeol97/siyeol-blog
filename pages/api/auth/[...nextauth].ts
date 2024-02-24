d590a0c815ea8b9aa415c6b1f35be34d024b39cf
siyeol97 <leesi2830@naver.com>
2024-02-24 20:22:03 +0900

    📦 Fix: add MongoDBAdapter

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,3 +1,5 @@
+import { connectDB } from '@/utils/database';
+import { MongoDBAdapter } from '@next-auth/mongodb-adapter';
 import NextAuth from 'next-auth';
 import GithubProvider from 'next-auth/providers/github';
 
@@ -9,5 +11,6 @@ export const authOptions = {
     }),
   ],
   secret: process.env.NEXT_AUTH_SECRET_KEY,
+  adapter: MongoDBAdapter(connectDB),
 };
 export default NextAuth(authOptions);

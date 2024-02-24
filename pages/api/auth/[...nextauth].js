3ea0dcc13942435893a47ed9c1f14afe8937df96
siyeol97 <leesi2830@naver.com>
2024-02-24 18:03:04 +0900

    🔧 Add: next-auth configure

---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,13 @@
+import NextAuth from 'next-auth';
+import GithubProvider from 'next-auth/providers/github';
+
+export const authOptions = {
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_SECRET_ID,
+      clientSecret: process.env.GITHUB_SECRET_KEY,
+    }),
+  ],
+  secret: process.env.NEXT_AUTH_SECRET_KEY,
+};
+export default NextAuth(authOptions);

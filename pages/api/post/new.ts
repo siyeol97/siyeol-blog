2b2474d2e560abf68cf3616bc3cc3bcaf82f9833
siyeol97 <leesi2830@naver.com>
2024-02-25 23:39:24 +0900

    🩹 Fix: write api 로직 수정

--- pages/api/post/new.ts
+++ pages/api/post/new.ts
@@ -8,22 +8,22 @@ export default async function handler(
   res: NextApiResponse
 ) {
   try {
-    if (req.method === 'GET') {
-      throw new Error('Invaild Access');
+    if (req.method !== 'POST') {
+      throw new Error();
     }
-    if (req.method === 'POST') {
-      if (req.body.title === '' || req.body.content === '') {
-        res.status(500).json('유효하지 않은 형식 입니다.');
-      }
-      const session = await getServerSession(req, res, authOptions);
-      const client = await connectDB;
-      const db = client.db('siyeol_blog');
-      const data = { ...req.body, author: session?.user?.email };
-      const result = await db.collection('blog_post').insertOne(data);
-      const { insertedId } = result;
-      res.redirect(302, `/post/${insertedId.toString()}`);
-      return;
+
+    if (req.body.title === '' || req.body.content === '') {
+      throw new Error();
     }
+
+    const session = await getServerSession(req, res, authOptions);
+    const client = await connectDB;
+    const db = client.db('siyeol_blog');
+    const data = { ...req.body, author: session?.user?.email };
+    const result = await db.collection('blog_post').insertOne(data);
+    const { insertedId } = result;
+    res.redirect(302, `/post/${insertedId.toString()}`);
+    return;
   } catch (error) {
     res.status(500).json({ error: 'failed to load' });
   }

fa6bdcccfda300639d9c83c70e7efa79f3a05c0f
siyeol97 <leesi2830@naver.com>
2024-02-29 19:24:19 +0900

    🚑 Fix: 메인 페이지 DB 연결

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,16 +1,18 @@
-import { headers } from 'next/headers';
+import { connectDB } from '@/utils/database';
 import Main from './Main';
 
+const getPostList = async () => {
+  const client = await connectDB;
+  const db = client.db('siyeol_blog');
+  const result = await db.collection('blog_post').find().toArray();
+  return result;
+};
+
 export default async function HomePage() {
-  const host = headers().get('host');
-  const protocol = process?.env.NODE_ENV === 'development' ? 'http' : 'https';
-  const result = await fetch(`${protocol}://${host}/api/post/pid`, {
-    cache: 'no-store',
-  });
-  const data: Post[] = await result.json();
+  const data = await getPostList();
   const postItem: Post[] = data.map((post) => {
     return {
-      _id: post._id,
+      _id: post._id.toString(),
       title: post.title,
       content: post.content,
       author: post.author,

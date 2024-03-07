dbea0cd903c35e5d10c645db47719cb220c37331
siyeol97 <leesi2830@naver.com>
2024-03-07 10:54:07 +0900

    🚑 Fix: 테스트 코드 제거

--- utils/getPostList.ts
+++ utils/getPostList.ts
@@ -1,7 +1,6 @@
 import { connectDB } from './database';
 
 const getPostList = async () => {
-  await new Promise((resolve) => setTimeout(resolve, 10000));
   const client = await connectDB;
   const db = client.db('siyeol_blog');
   const result = await db.collection('blog_post').find().toArray();

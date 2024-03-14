3d99e07def4e4a44c85a9a24afc6679b70e5dc54
siyeol97 <leesi2830@naver.com>
2024-03-14 21:49:20 +0900

    💄 Style: 유저 정보 모달 추가

--- components/MainLogo.tsx
+++ components/MainLogo.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 export default function MainLogo() {
   return (
     <h4>
-      <Link href={'/'}>Siyeol&apos;s Blog</Link>
+      <Link href={'/'}>Siyeol&apos;s Dev-log</Link>
     </h4>
   );
 }
--- components/UserInfo.tsx
+++ components/UserInfo.tsx
@@ -6,8 +6,14 @@ import Image from 'next/image';
 import Link from 'next/link';
 import DefaultAvatar from './DefaultAvatar';
 import styles from './UserInfo.module.css';
+import { useState } from 'react';
 
 export default function UserInfo({ session }: { session: Session }) {
+  const [isToggle, setIsToggle] = useState(false);
+  const handleToggle = () => {
+    setIsToggle((prev) => !prev);
+  };
+
   return (
     <div className={styles.wrapper}>
       <Link
@@ -16,25 +22,42 @@ export default function UserInfo({ session }: { session: Session }) {
       >
         새 글 쓰기
       </Link>
-      {session.user?.image ? (
-        <div className={styles.avatar}>
-          <Image
-            src={session.user.image}
-            alt='avatar'
-            fill
-            sizes='40px'
-          />
-        </div>
-      ) : (
-        <DefaultAvatar name={session.user!.name?.slice(-2)} />
-      )}
-      <button
-        onClick={() => {
-          signOut();
-        }}
+      <div
+        className={styles.user_session}
+        onClick={handleToggle}
       >
-        로그아웃
-      </button>
+        {session.user?.image ? (
+          <div className={styles.avatar}>
+            <Image
+              src={session.user.image}
+              alt='avatar'
+              fill
+              sizes='35px'
+            />
+          </div>
+        ) : (
+          <div style={{ cursor: 'pointer' }}>
+            <DefaultAvatar name={session.user!.name?.slice(-2)} />
+          </div>
+        )}
+        {isToggle ? (
+          <div className={styles.user_menu}>
+            <div className={styles.user_menu_box}>
+              <p className={styles.user_menu_name}>{session.user?.name}</p>
+              <p className={styles.user_menu_email}>{session.user?.email}</p>
+            </div>
+            <hr />
+            <button
+              onClick={() => {
+                signOut();
+              }}
+              className={styles.logout_button}
+            >
+              로그아웃
+            </button>
+          </div>
+        ) : null}
+      </div>
     </div>
   );
 }

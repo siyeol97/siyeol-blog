ca92d77937e9ea692e0594e9a0964997a0790a96
siyeol97 <leesi2830@naver.com>
2024-03-29 22:23:44 +0900

    💄 Style: 글 목록 스켈레톤 UI

--- app/(home)/components/LoadingCard.tsx
+++ app/(home)/components/LoadingCard.tsx
@@ -1,5 +1,54 @@
+import Skeleton from './Skeleton';
 import styles from '../css/LoadingCard.module.css';
 
 export default function LoadingCard() {
-  return <div className={styles.loading_card}></div>;
+  return (
+    <>
+      <hr className={styles.separator} />
+      <div className={styles.card}>
+        <div className={styles.card_preview}>
+          <div className={styles.card_preview_text}>
+            <Skeleton
+              width={'370px'}
+              height={'40px'}
+            />
+            <Skeleton
+              width={'100%'}
+              height={'14px'}
+            />
+            <Skeleton
+              width={'100%'}
+              height={'14px'}
+            />
+            <Skeleton
+              width={'100%'}
+              height={'14px'}
+            />
+          </div>
+          <div className={styles.thumbnail}>
+            <Skeleton
+              width={'130px'}
+              height={'130px'}
+            />
+          </div>
+        </div>
+        <div className={styles.card_bottom}>
+          <Skeleton
+            width={'120px'}
+            height={'14px'}
+          />
+          <div className={styles.social}>
+            <Skeleton
+              width={'30px'}
+              height={'14px'}
+            />
+            <Skeleton
+              width={'30px'}
+              height={'14px'}
+            />
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
--- app/(home)/loading.tsx
+++ app/(home)/loading.tsx
@@ -2,7 +2,7 @@ import LoadingCard from './components/LoadingCard';
 import styles from './css/loading.module.css';
 
 export default function loading() {
-  const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  const arr = [1, 2, 3, 4];
   return (
     <section className={styles.section}>
       <ul className={styles.ul}>

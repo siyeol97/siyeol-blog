9623d48c4907e1ddaff459caae09dd046866a3b4
siyeol97 <leesi2830@naver.com>
2024-05-20 17:32:06 +0900

    🩹 Fix: middleware 글 쓰기 redirect

--- middleware.ts
+++ middleware.ts
@@ -7,12 +7,17 @@ export async function middleware(request: NextRequest) {
     request.nextUrl.pathname.startsWith('/edit')
   ) {
     const session = await getToken({ req: request });
+    const isAuthorized = session?.email === 'leesi2830@naver.com';
+    if (!isAuthorized) {
+      return NextResponse.redirect('https://siyeol-blog.vercel.app/');
+    }
     if (!session) {
       return NextResponse.redirect(
         'https://siyeol-blog.vercel.app/auth/signin'
       );
     }
   }
+
   if (request.nextUrl.pathname.startsWith('/auth/signin')) {
     const session = await getToken({ req: request });
     if (session) {

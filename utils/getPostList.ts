52b3d5eb4030f4303cc2acbab5833c38cb81ff55
siyeol97 <leesi2830@naver.com>
2024-05-21 11:47:04 +0900

    🐛 Fix: 새 글을 쓴 후 메인페이지에 반영되지 않는 버그수정(no-cache)

--- utils/getPostList.ts
+++ utils/getPostList.ts
@@ -2,7 +2,9 @@ import { Post } from '@/app/type';
 
 const getPostList = async (): Promise<Post[] | null> => {
   try {
-    const response = await fetch(`${process.env.API_URL}/api/post/pid`);
+    const response = await fetch(`${process.env.API_URL}/api/post/pid`, {
+      cache: 'no-cache',
+    });
     const data: Post[] = await response.json();
     return data;
   } catch (error) {

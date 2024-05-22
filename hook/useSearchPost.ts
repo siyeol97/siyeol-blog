a429224b1006b511ee4c9435ac1f1e1a7655045d
siyeol97 <leesi2830@naver.com>
2024-05-22 17:53:16 +0900

    ✨ Feat: 포스트 검색 debounce 적용

--- hook/useSearchPost.ts
+++ hook/useSearchPost.ts
@@ -1,19 +1,27 @@
 import { Post } from '@/app/type';
-import { ChangeEvent, useState } from 'react';
+import { ChangeEvent, useEffect, useState } from 'react';
 
 const useSearchPost = (data: Post[]) => {
   const [searchValue, setSearchValue] = useState('');
   const [searchedData, setSearchedData] = useState<Post[]>(data);
 
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      const filteredPosts = data.filter((post) =>
+        post.title
+          .replace(/\s/g, '')
+          .toLowerCase()
+          .includes(searchValue.replace(/\s/g, '').toLowerCase())
+      );
+      setSearchedData(filteredPosts);
+    }, 400);
+
+    return () => clearTimeout(timer);
+  }, [searchValue, data]);
+
   const onChange = (e: ChangeEvent<HTMLInputElement>) => {
-    setSearchValue(e.target.value);
-    const filteredPosts = data.filter((post) =>
-      post.title
-        .replace(/\s/g, '')
-        .toLowerCase()
-        .includes(e.target.value.replace(/\s/g, '').toLowerCase())
-    );
-    setSearchedData(filteredPosts);
+    const value = e.target.value;
+    setSearchValue(value);
   };
 
   return { searchValue, searchedData, onChange };

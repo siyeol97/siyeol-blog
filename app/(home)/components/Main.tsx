be5bc5660cfb438c26b7dc9d71202e120260e40f
siyeol97 <leesi2830@naver.com>
2024-05-22 18:44:24 +0900

    💄 Style: 검색창 UI 제작

--- app/(home)/components/Main.tsx
+++ app/(home)/components/Main.tsx
@@ -4,37 +4,50 @@ import { Post } from '@/app/type';
 import styles from '../css/Main.module.css';
 import Card from './Card';
 import useSearchPost from '@/hook/useSearchPost';
+import Image from 'next/image';
 
 export default function Main({ data }: { data: Post[] }) {
   const { searchValue, searchedData, onChange } = useSearchPost(data);
 
   return (
     <section className={styles.section}>
-      <div className={styles.search_section}>
-        <input
-          type='text'
-          className={styles.search}
-          value={searchValue}
-          onChange={onChange}
-        />
-        <p
-          className={styles.search_text}
-          style={{ visibility: searchValue ? 'visible' : 'hidden' }}
-        >
-          <span className={styles.search_value}>
-            &lsquo;{searchValue}&rsquo;
-          </span>
-          &nbsp;검색 결과입니다.
-        </p>
+      <div className={styles.wrapper}>
+        <div className={styles.search_section}>
+          <div className={styles.input_container}>
+            <input
+              type='text'
+              className={styles.search_input}
+              value={searchValue}
+              onChange={onChange}
+              placeholder='포스트를 검색해 보세요'
+            />
+            <Image
+              src={'/search-icon.svg'}
+              alt='search-icon'
+              width={24}
+              height={24}
+              className={styles.search_icon}
+            />
+          </div>
+          <p
+            className={styles.search_text}
+            style={{ visibility: searchValue ? 'visible' : 'hidden' }}
+          >
+            <span className={styles.search_value}>
+              &lsquo;{searchValue}&rsquo;
+            </span>
+            &nbsp;검색 결과입니다.
+          </p>
+        </div>
+        <ul className={styles.ul}>
+          {searchedData?.map((post) => (
+            <Card
+              key={post._id.toString()}
+              post={post}
+            />
+          ))}
+        </ul>
       </div>
-      <ul className={styles.ul}>
-        {searchedData?.map((post) => (
-          <Card
-            key={post._id.toString()}
-            post={post}
-          />
-        ))}
-      </ul>
     </section>
   );
 }

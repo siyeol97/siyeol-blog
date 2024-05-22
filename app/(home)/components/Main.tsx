1ad78652696aff7c0434e4662f1544cb1e41c7cb
siyeol97 <leesi2830@naver.com>
2024-05-22 17:19:51 +0900

    ✨ Feat: 포스트 검색 기능

--- app/(home)/components/Main.tsx
+++ app/(home)/components/Main.tsx
@@ -3,12 +3,44 @@
 import { Post } from '@/app/type';
 import styles from '../css/Main.module.css';
 import Card from './Card';
+import { ChangeEvent, useState } from 'react';
 
 export default function Main({ data }: { data: Post[] }) {
+  const [searchValue, setSearchValue] = useState('');
+  const [searchedData, setSearchedData] = useState<Post[]>(data);
+
+  const onChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setSearchValue(e.target.value);
+    const filteredPosts = data.filter((post) =>
+      post.title
+        .replace(/\s/g, '')
+        .toLowerCase()
+        .includes(e.target.value.replace(/\s/g, '').toLowerCase())
+    );
+    setSearchedData(filteredPosts);
+  };
+
   return (
     <section className={styles.section}>
+      <div className={styles.search_section}>
+        <input
+          type='text'
+          className={styles.search}
+          value={searchValue}
+          onChange={onChange}
+        />
+        <p
+          className={styles.search_text}
+          style={{ visibility: searchValue ? 'visible' : 'hidden' }}
+        >
+          <span className={styles.search_value}>
+            &lsquo;{searchValue}&rsquo;
+          </span>
+          &nbsp;검색 결과입니다.
+        </p>
+      </div>
       <ul className={styles.ul}>
-        {data?.map((post) => (
+        {searchedData?.map((post) => (
           <Card
             key={post._id.toString()}
             post={post}

d5aec2a7529d8a15122e9b19e80529cfb98254cf
siyeol97 <leesi2830@naver.com>
2024-05-26 11:57:40 +0900

    ♻️ Refactor: 랜딩페이지 텍스트 분리

--- constants/landingText.ts
+++ constants/landingText.ts
@@ -1,6 +1,19 @@
 export const TEXT_START_X = 50;
-export const TEXT_START_Y = 300;
+export const TEXT_START_Y = 50;
 export const TEXT_LINE_HEIGHT = 24;
 export const FONT_FAMILY = 'monospace';
 export const FONT_SIZE = 16;
 export const FONT_COLOR = '#000';
+export const TEXT = `class Developer {
+  constructor(name, techStack) {
+      this.name = name;
+      this.techStack = techStack;
+  }
+
+  getGreetings() {
+      return '안녕하세요. 개인블로그에 오신 것을 환영합니다!';
+  }
+}
+
+const FE_Developer = new Developer('이시열', ['JavaScript', 'TypeScript', 'React', 'Next.js']);
+console.log(FE_Developer.getGreetings());`;
--- utils/animateLandingText.ts
+++ utils/animateLandingText.ts
@@ -2,13 +2,13 @@ import {
   FONT_COLOR,
   FONT_FAMILY,
   FONT_SIZE,
+  TEXT,
   TEXT_LINE_HEIGHT,
   TEXT_START_X,
   TEXT_START_Y,
 } from '@/constants/landingText';
 
 const animateLandingText = (context: CanvasRenderingContext2D) => {
-  const text = `const greeting = 'Hello, world!';\nconsole.log(greeting);`;
   let index = 0;
   let showCursor = true;
   let lastTime = 0;
@@ -20,8 +20,9 @@ const animateLandingText = (context: CanvasRenderingContext2D) => {
     context.font = `${FONT_SIZE}px ${FONT_FAMILY}`;
     context.fillStyle = FONT_COLOR;
 
-    const currentText = text.slice(0, index);
+    const currentText = TEXT.slice(0, index);
     const lines = currentText.split('\n');
+
     lines.forEach((line, i) => {
       context.fillText(line, TEXT_START_X, TEXT_START_Y + i * TEXT_LINE_HEIGHT);
     });
@@ -34,7 +35,7 @@ const animateLandingText = (context: CanvasRenderingContext2D) => {
       context.fillRect(cursorX, cursorY - 15, 10, 20);
     }
 
-    if (index >= text.length) {
+    if (index >= TEXT.length) {
       if (timestamp - lastTime > cursorBlinkInterval) {
         showCursor = !showCursor;
         lastTime = timestamp;

1abe9be67b739e9745c32ba72fc1e62f4647d0b0
siyeol97 <leesi2830@naver.com>
2024-11-22 17:27:06 +0900

    ✨ Feat: enhance PostTag component to accept dynamic tags and update styles

--- app/post/[id]/components/PostTag.tsx
+++ app/post/[id]/components/PostTag.tsx
@@ -1,5 +1,19 @@
+import Tag from '@/components/Tag';
 import styles from '../css/PostTag.module.css';
 
-export default function PostTag() {
-  return <div className={styles.wrapper}>#react #next.js</div>;
+interface Props {
+  tags: string[];
+}
+
+export default function PostTag({ tags }: Props) {
+  return (
+    <div className={styles.wrapper}>
+      {tags.map((tag) => (
+        <Tag
+          tag={tag}
+          key={tag}
+        />
+      ))}
+    </div>
+  );
 }
--- components/Tag.tsx
+++ components/Tag.tsx
@@ -1,9 +1,11 @@
+import { memo } from 'react';
 import styles from './Tag.module.css';
 
 interface Props {
   tag: string;
 }
 
-export default function Tag({ tag }: Props) {
+function Tag({ tag }: Props) {
   return <span className={styles.tag}>{tag}</span>;
 }
+export default memo(Tag);

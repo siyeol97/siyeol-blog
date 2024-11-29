53e7926bca7d052be6b5b6a500565e2075015379
siyeol97 <leesi2830@naver.com>
2024-11-29 17:17:20 +0900

    ✨ Feat: add rehype-sanitize for Markdown security and customize schema to restrict certain tags

--- components/Markdown/MarkdownView.tsx
+++ components/Markdown/MarkdownView.tsx
@@ -3,16 +3,34 @@ import remarkGfm from 'remark-gfm';
 import rehypeRaw from 'rehype-raw';
 import CodeBlock from './CodeBlock';
 import styles from './MarkdownView.module.css';
+import rehypeSanitize, { defaultSchema } from 'rehype-sanitize';
 
 interface Props {
   content: string;
 }
 
+const customSchema = {
+  ...defaultSchema,
+  tagNames:
+    defaultSchema.tagNames?.filter(
+      (tag) =>
+        ![
+          'iframe',
+          'script',
+          'object',
+          'embed',
+          'form',
+          'link',
+          'style',
+        ].includes(tag)
+    ) || [],
+};
+
 export default function MarkdownView({ content }: Props) {
   return (
     <Markdown
       remarkPlugins={[remarkGfm]}
-      rehypePlugins={[rehypeRaw]}
+      rehypePlugins={[rehypeRaw, [rehypeSanitize, customSchema]]}
       components={{
         //pre: MarkdownHeader,
         code: CodeBlock,
